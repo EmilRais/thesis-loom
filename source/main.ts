0c8e736eddcf08160fcd48dbdd7111f57c9984bd
Emil Rais <emilrais@outlook.com>
2017-10-22 11:11:03 +0200

    Now able to load specification from main.ts

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,10 @@
-console.log("Something");
+import { IO } from "./io";
+
+const io = new IO();
+
+const parameters = process.argv.slice(2);
+const specificationPath = parameters[0];
+
+io.loadSpecification(specificationPath)
+    .then(console.log)
+    .catch(console.error);
